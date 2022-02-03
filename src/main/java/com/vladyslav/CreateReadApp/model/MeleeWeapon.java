d6a59567ea099344af55eaf035f61d847faeece2
Vlad Bezdolniy <spiring110@ukr.net>
2022-02-04 01:56:17 +0200

    equal melee

--- src/main/java/com/vladyslav/CreateReadApp/model/MeleeWeapon.java
+++ src/main/java/com/vladyslav/CreateReadApp/model/MeleeWeapon.java
@@ -5,6 +5,7 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import java.util.Objects;
 
 @Entity
 @DiscriminatorValue("MELEE_WEAPON")
@@ -17,5 +18,19 @@ public class MeleeWeapon extends Item{
     public MeleeWeapon(String name, double price, String description, String imageUrl, Dice meleeAttackDice) {
         super(name, price, description, imageUrl);
         this.meleeAttackDice = meleeAttackDice;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        MeleeWeapon that = (MeleeWeapon) o;
+        return meleeAttackDice == that.meleeAttackDice;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), meleeAttackDice);
     }
 }
