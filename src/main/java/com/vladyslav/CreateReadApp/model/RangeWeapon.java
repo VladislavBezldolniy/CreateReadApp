b73bd2c49f446cd616f6eef567cff94969752001
Vlad Bezdolniy <spiring110@ukr.net>
2022-02-04 06:11:16 +0200

    ManyToOne relation

--- src/main/java/com/vladyslav/CreateReadApp/model/RangeWeapon.java
+++ src/main/java/com/vladyslav/CreateReadApp/model/RangeWeapon.java
@@ -1,15 +1,44 @@
 package com.vladyslav.CreateReadApp.model;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 @Entity
 @DiscriminatorValue(value = "RANGE_WEAPON")
+@NoArgsConstructor
 public class RangeWeapon extends Item {
 
-    public RangeWeapon(){}
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ammo_id")
+    Ammo ammo;
 
-    public RangeWeapon(String name, double price, String description, String imageUrl) {
+    public RangeWeapon(String name, double price, String description, String imageUrl, Ammo ammo) {
         super(name, price, description, imageUrl);
+        this.ammo = ammo;
+    }
+
+    public Ammo getAmmo() {
+        return ammo;
+    }
+    public void setAmmo(Ammo ammo) {
+        this.ammo = ammo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        RangeWeapon that = (RangeWeapon) o;
+        return ammo.equals(that.ammo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), ammo);
     }
 }
