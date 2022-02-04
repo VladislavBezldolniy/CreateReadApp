71c364c0b0c318c3f6ed6fb6f4b1d28937743e2c
Vlad Bezdolniy <spiring110@ukr.net>
2022-02-04 05:35:53 +0200

    genereting raw data

--- src/main/java/com/vladyslav/CreateReadApp/CreateReadAppApplication.java
+++ src/main/java/com/vladyslav/CreateReadApp/CreateReadAppApplication.java
@@ -1,13 +1,32 @@
 package com.vladyslav.CreateReadApp;
 
+import com.vladyslav.CreateReadApp.model.Ammo;
+import com.vladyslav.CreateReadApp.model.Dice;
+import com.vladyslav.CreateReadApp.model.MeleeWeapon;
+import com.vladyslav.CreateReadApp.model.RangeWeapon;
+import com.vladyslav.CreateReadApp.repo.AmmoRepo;
+import com.vladyslav.CreateReadApp.service.ItemServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+
+import static com.vladyslav.CreateReadApp.model.Ammo.Caliber.Caliber9mm;
 
 @SpringBootApplication
 public class CreateReadAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CreateReadAppApplication.class, args);
-	}
+		ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(CreateReadAppApplication.class, args);
+		ItemServiceImpl itemService = configurableApplicationContext.getBean(ItemServiceImpl.class);
 
+		/*itemService.create(new MeleeWeapon("Katana", 395, "Bite me", "", Dice.D12));
+		itemService.create(new RangeWeapon("M24", 2345.4, "Protec", ""));
+		*/
+
+		AmmoRepo ammoRepository = configurableApplicationContext.getBean(AmmoRepo.class);
+		Arrays.asList(Ammo.Caliber.values()).forEach(caliber -> ammoRepository.save(new Ammo(caliber)));
+
+	}
 }
